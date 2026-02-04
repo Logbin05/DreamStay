c9372001837208d0e2dbebc356b0b34b02e02194
Алексей Куриганов <demo1fort@gmail.com>
2026-02-04 19:16:34 +0300

    Updated: styles

--- src/layout/main/footer.tsx
+++ src/layout/main/footer.tsx
@@ -1,9 +1,11 @@
 export function Footer() {
   return (
     <>
-      <footer className="w-screen bg-primary-color flex justify-center items-center
-      h-50 p-2 drop-shadow-accent-color drop-shadow-2xl">
-        <span className="text-accent-color text-2xl">
+      <footer
+        className="w-screen bg-gray-700/30 flex justify-center items-center
+      h-50 p-2 drop-shadow-accent-color drop-shadow-2xl"
+      >
+        <span className="text-white text-2xl">
           COPYRIGHT © 2026 DreamStay LTD. ALL RIGHTS RESERVED
         </span>
       </footer>
--- src/layout/main/header.tsx
+++ src/layout/main/header.tsx
@@ -1,21 +1,17 @@
 export function Header() {
   return (
-    <header className="w-screen bg-primary-color h-[120px] flex flex-row gap-5 justify-around items-center shadow-accent-color shadow-2xl">
+    <header className="z-10 bg-gray-700/30 w-screen h-[120px] flex flex-row gap-5 justify-around items-center">
       <div className="">
-        <span className="text-accent-color text-4xl">DreamStay</span>
-        <span className="text-text-muted text-2xl font-medium"> − </span>
-        <span className="text-accent-active text-xl">
-          Место, где отдыхают мечты
-        </span>
+        <span className="text-white text-4xl">DreamStay</span>
       </div>
       <ul className="flex flex-row gap-10">
         <li>
-          <button className="bg-secondary-color text-lg p-2 text-text-color transition-all rounded-sm active:rounded-xl">
+          <button className="backdrop-blur-2xl text-lg px-5 py-2 border-gray-800/30 border-2 text-white rounded-2xl transition-all active:scale-120">
             Войти
           </button>
         </li>
         <li>
-          <button className="bg-secondary-color text-lg p-2 text-text-color transition-all rounded-sm active:rounded-xl">
+          <button className="backdrop-blur-2xl text-lg px-5 py-2 border-gray-800/30 border-2 text-white rounded-2xl transition-all active:scale-120">
             Зарегистрироваться
           </button>
         </li>
