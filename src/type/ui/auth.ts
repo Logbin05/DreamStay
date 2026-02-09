3f68c5fa2b80885cafed615ade7b6c4ebb0e11e6
Logbin05 <demo1fort@gmail.com>
2026-02-09 19:04:27 +0300

    Added: new type for forms register

--- src/type/ui/auth.ts
+++ src/type/ui/auth.ts
@@ -1,3 +1,10 @@
+import type { RegisterFormData } from "@type/user_auth";
+
 export interface AuthPopUp {
   onAction?: () => void;
+}
+
+export interface RegisterPopUpProps {
+  onClose: () => void;
+  onSubmit?: (data: RegisterFormData) => void;
 }
